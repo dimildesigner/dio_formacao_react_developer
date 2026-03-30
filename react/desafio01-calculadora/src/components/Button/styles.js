0e90198c9e61774d9f93e223cc7cfcec543c0cf3
dimildesigner <dimildesignerdev@gmail.com>
2026-03-29 23:04:20 -0300

    Novo desafio de criação da calculadora no React.

---
+++ react/desafio01-calculadora/src/components/Button/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: 1px solid #cdcdcd;
+    background-color: #98c790;
+    color: #273325;
+    font-famyle: "Roboto";    
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+`
---
+++ react/desafio01-calculadora/src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #5f675f;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-family: 'Roboto';
+    font-size: 24px;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #5f675f;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        
+        font-family: 'Roboto';
+        font-size: 24px;
+        color: #ffffff
+    }
+`
